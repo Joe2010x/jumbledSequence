18574859d295811fc6293da75db7614653bf2888
Joe Sun <dps_swanuk@hotmail.com>
2023-07-21 17:38:57 +0200

    setup

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace JumbledSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sequence = new string [] {"none","+","+","-","+","-"};
+            Console.WriteLine("testRun");
+        }
+    }
+}
